755f1cf5d43733501ffb129524b1bef2c8c2d291
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-01-24 01:29:10 +0100

    add additional method example

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,8 +1,19 @@
+import * as bar from 'bar/some-file';
+import * as foo from 'foo/another-file';
+
+export function asMethodsFromFiles () {
+
+  const one = bar.someFileFromBar();
+  const two = foo.noticeTheImportPath();
+
+  return { one, two };
+
+}
 
 export function someMethod () {
   return 'someMethod';
 }
 
 export function anotherMethod () {
-  return 'another method';
+  return 'anotherMethod';
 }
